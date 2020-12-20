c5bc16cda41d5ae2fc889b2db267b2ded2461ee0
BehnamEbrahimi <behnam.ebrahimi@gmail.com>
2020-12-20 20:06:35 +1100

    Updated

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import { HeaderTranslationContext } from "../contexts/TranslationsContexts";
 import styles from "./NavBar.module.scss";
 import MenuItem from "./MenuItem";
@@ -7,8 +8,8 @@ import MenuItem from "./MenuItem";
 interface Props {}
 
 const NavBar: NextPage<Props> = () => {
+  const router = useRouter();
   const headerTranslationContext = useContext(HeaderTranslationContext);
-  const [activeTab, setActiveTab] = useState("home");
 
   return (
     <div className={`container ${styles.wrapper}`}>
@@ -19,28 +20,18 @@ const NavBar: NextPage<Props> = () => {
       </div>
       <nav>
         <ul className={styles.list}>
-          <li
-            className={styles["list-item"]}
-            onClick={() => {
-              setActiveTab("home");
-            }}
-          >
+          <li className={styles["list-item"]}>
             <MenuItem
               text={headerTranslationContext["back-to-home"]}
               to="/"
-              active={activeTab === "home"}
+              active={router.pathname === "/"}
             />
           </li>
-          <li
-            className={styles["list-item"]}
-            onClick={() => {
-              setActiveTab("about");
-            }}
-          >
+          <li className={styles["list-item"]}>
             <MenuItem
               text={headerTranslationContext["go-to-about"]}
               to="/about"
-              active={activeTab === "about"}
+              active={router.pathname === "/about"}
             />
           </li>
         </ul>
